29a76cab4652c2e82b3dceb8e7a6ee3f6f66c2ad
Shivam Savani <shivambsavani@gmail.com>
2025-04-27 18:21:45 +1000

    changelog: faster build time

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,17 @@ export default defineConfig({
     open: false,
     allowedHosts: ['localhost', '127.0.0.1', '.ngrok-free.app'],
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const parts = id.toString().split('node_modules/')[1].split('/');
+            return parts[0];
+          }
+        },
+      },
+    },
+  },
 });
